b1ad21d432e2649e4cd0d976f73cb6bda1801480
Timothy Boronczyk <tboronczyk@gmail.com>
2012-10-20 19:53:29 -0400

    Organizing files in directories

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,6 +1,6 @@
 <?php
-require_once "vendor/autoload.php";
-require_once "User.php";
+require_once "../vendor/autoload.php";
+require_once "../include/User.php";
 
 class UserTest extends PHPUnit_Framework_TestCase
 {
